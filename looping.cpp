e14040437d10e2faabdd6a32f763e5603acc4877
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-03-31 21:20:39 +0700

    deklarasi variable x

---
+++ looping.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main (){
+    int x;
+
+    //perulangan dengan FOR
+    //mencetak tulisan sebanyak 5 kali
+    cout << "PERULANGAN FOR" << endl;
